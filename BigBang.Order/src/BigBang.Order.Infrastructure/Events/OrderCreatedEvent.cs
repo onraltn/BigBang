11112f061039b186024a6eb8819c492c578e3f95
Onur Altun <onur94altun@gmail.com>
2023-08-07 22:17:35 +0300

    evetns

---
+++ BigBang.Order/src/BigBang.Order.Infrastructure/Events/OrderCreatedEvent.cs
@@ -0,0 +1,19 @@
+﻿using BigBang.Order.Infrastructure.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigBang.Events
+{
+    public record OrderCreatedEvent : IEvent
+    {
+        public Guid CollerationId { get; set; }
+        public string TrackingNumber { get; set; }
+        public string CustomerName { get;set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CreateDate { get; set; }
+    }
+}
